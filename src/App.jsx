aca724c80e8e6ae31270167568d680fa96fe914c
Gerardotango <135970009+Gerardotango@users.noreply.github.com>
2023-11-25 09:38:40 +0000

    add_new _header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Button, Counter } from "./components/component";
 
 export default function App() {
-  const [value, setValue] = useState(0) 
+  const [value, setValue] = useState(0)
 
   const resetCounter = () => {
     setValue(0)
   }
 
-  const incrementCounter = () => {    
-    setValue(value+1)
+  const incrementCounter = () => {
+    setValue(value + 1)
   }
 
   const decrementCounter = () => {
-    setValue(value-1)
+    setValue(value - 1)
   }
   return (
     <main>
-      <div>            
-     <Button onClick={resetCounter}>Reset</Button>  
-      </div>      
+      <h1>hello</h1>
+      <div>
+        <Button onClick={resetCounter}>Reset</Button>
+      </div>
       <Button onClick={decrementCounter}>-</Button>
       <Counter value={value} />
       <Button onClick={incrementCounter}>+</Button>
